9d6a27588e5365b0cd2d0a48c603fce312c4bec0
kno19005 <kno19005@byui.edu>
2020-05-15 13:37:26 -0600

    Updated with my message.

--- src/brookhart/Main.java
+++ src/brookhart/Main.java
@@ -3,6 +3,9 @@ package brookhart;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello, my name is Chris.");
+	    System.out.println("Hello, my name is Chris.");
+
+        System.out.println("Hello Chris, my name is Tom.");
+
     }
 }
