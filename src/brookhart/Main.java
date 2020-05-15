9bcc98012e1e0d67926db4f54de014e84fccb13e
Chris Brookhart <bro19056@byui.edu>
2020-05-15 11:33:26 -0600

    Update Main.java
    Change name to Christopher.

--- src/brookhart/Main.java
+++ src/brookhart/Main.java
@@ -3,6 +3,6 @@ package brookhart;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello, my name is Chris.");
+	System.out.println("Hello, my name is Christopher.");
     }
 }
